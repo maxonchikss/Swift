399aaca7d6848e6285a22a1fed0e8eb17b486476
Maxonchiks <nicichin.max@yandex.ru>
2026-02-17 18:01:06 +0300

    Delete old Holdem directory

--- Holdem/main.swift
+++
@@ -1,9 +0,0 @@
-//переход с дабл на инт
-//интегрировать систему авто алл ина и обработать онуление стеков
-//смена позиций
-//сделать бота
-import Foundation
-
-var newGame = PokerGame(BigBlindes: 1000)
-newGame.startGame()
-
